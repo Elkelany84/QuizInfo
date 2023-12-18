fcbf9bd922d78721f9fc54e80f9089b84a5eeff2
unknown <drnet80@gmail.com>
2023-12-18 20:39:15 +0200

    part1_time15.36

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,38 +14,83 @@ class _HomeState extends State<Home> {
       body: Container(
         child: Column(
           children: [
-            Container(
-              height: 220,
-              padding: EdgeInsets.only(top: 50, left: 20),
-              decoration: BoxDecoration(color: Color(0xFF2a2b31)),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(60),
-                    child: Image.asset(
-                      "images/boy.jpg",
-                      height: 50,
-                      width: 50,
-                      fit: BoxFit.cover,
+            Stack(children: [
+              Container(
+                height: 220,
+                padding: EdgeInsets.only(top: 50, left: 20),
+                decoration: BoxDecoration(
+                    color: Color(0xFF2a2b31),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(60),
+                      child: Image.asset(
+                        "images/boy.jpg",
+                        height: 50,
+                        width: 50,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10.0),
-                    child: Text(
-                      "Ahmed Elkelany",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
+                    SizedBox(
+                      width: 20,
                     ),
-                  )
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        "Ahmed Elkelany",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.only(top: 120, left: 20, right: 20),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20)),
+                      child: Image.asset("images/quiz.PNG"),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Play &\nWin",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Play Game by Guessing \nthe Image",
+                          style: TextStyle(
+                              color: Color(0xFFa4a4a4),
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ])
           ],
         ),
       ),
