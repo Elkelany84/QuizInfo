c8624bcf751b319af778fea0d27646c90f10cdf0
Elkelany84 <drnet80@gmail.com>
2023-12-21 11:08:00 +0200

    EndPart1

--- lib/pages/question.dart
+++ lib/pages/question.dart
@@ -44,24 +44,109 @@ class _QuestionState extends State<Question> {
                 ],
               ),
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30))),
-              child: Column(
-                children: [
-                  Image.asset(
-                    "images/ronaldo.jpg",
-                    height: 300,
-                    width: MediaQuery.of(context).size.width,
-                    fit: BoxFit.cover,
-                  )
-                ],
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30))),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 20.0, left: 20, top: 40),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          "images/ronaldo.jpg",
+                          height: 300,
+                          width: MediaQuery.of(context).size.width,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Color(0xFF818181), width: 1.5),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Christiano Ronaldo",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Color(0xFF818181), width: 1.5),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Lionel Messi",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Color(0xFF818181), width: 1.5),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Neymar",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                        padding: EdgeInsets.all(15),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Color(0xFF818181), width: 1.5),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Text(
+                          "Elninny",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            )
+            ),
           ],
         ),
       ),
